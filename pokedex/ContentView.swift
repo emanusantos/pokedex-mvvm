2289ff42c0c674716d4d4214959e3593d0aa2c34
emanusantos <emanuel.lmds1@gmail.com>
2023-09-16 19:20:03 -0300

    first commit

---
+++ pokedex/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  pokedex
+//
+//  Created by Emanuel Santos on 16/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by Emanuel Santos on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
