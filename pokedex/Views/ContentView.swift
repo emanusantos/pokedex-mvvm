94547fc807d743618c34306629b7eb55754e78bc
emanusantos <emanuel.lmds1@gmail.com>
2023-09-16 22:14:47 -0300

    finished app

--- pokedex/Views/ContentView.swift
+++ pokedex/Views/ContentView.swift
@@ -10,10 +10,25 @@ import SwiftUI
 struct ContentView: View {
     @StateObject var vm = ViewModel();
     
+    private let adaptiveColumns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
         NavigationView {
-            Text("Hello, world!")
-                .padding()
+            ScrollView {
+                LazyVGrid(columns: adaptiveColumns, spacing: 10) {
+                    ForEach(vm.filteredPokemon) { pokemon in
+                        NavigationLink(destination: PokemonDetailView(pokemon: pokemon)) {
+                            PokemonView(pokemon: pokemon)
+                        }
+                    }
+                }
+                .animation(.easeIn(duration: 0.3), value: vm.filteredPokemon.count)
+                .navigationTitle("PokemonUI")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .searchable(text: $vm.searchText)
         }
         .environmentObject(vm);
     }
